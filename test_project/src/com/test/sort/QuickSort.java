76a2baa2d622ca5e7b0c3ed18bcc351d0c574564
Tomas Aubrecht <7473-1@unicornuniverse.eu>
2014-02-18 22:47:17 +0100

    Refactored variable name.

--- test_project/src/com/test/sort/QuickSort.java
+++ test_project/src/com/test/sort/QuickSort.java
@@ -7,19 +7,19 @@ public class QuickSort {
 	  int temp = list[pivot]; // prohozeni pivotu s poslednim prvkem
 	  list[pivot] = list[right];
 	  list[right] = temp;
-	  int i = left;
+	  int iter = left;
 	  for (int j = left; j < right; j++) {
 	    if (list[j] < list[right]) { // prvek je mensi, nez pivot
-	      temp = list[i]; // prohozeni pivotu s prvkem na pozici
-	      list[i] = list[j];
+	      temp = list[iter]; // prohozeni pivotu s prvkem na pozici
+	      list[iter] = list[j];
 	      list[j] = temp;
-	      i++; // posun pozice
+	      iter++; // posun pozice
 	    }
 	  }
-	  temp = list[i]; // prohozeni pivotu zpet
-	  list[i] = list[right];
+	  temp = list[iter]; // prohozeni pivotu zpet
+	  list[iter] = list[right];
 	  list[right] = temp;
-	  return i; // vrati novy index pivotu
+	  return iter; // vrati novy index pivotu
 	}
 
 	public static void limited_quicksort(int[] list, int left, int right) {

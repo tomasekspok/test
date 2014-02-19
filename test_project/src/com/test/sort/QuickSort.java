55496801263164192fb5641f31e13f8094843cd1
Tomas Aubrecht <7473-1@unicornuniverse.eu>
2014-02-19 09:34:21 +0100

    Added javadoc comment blocks

--- test_project/src/com/test/sort/QuickSort.java
+++ test_project/src/com/test/sort/QuickSort.java
@@ -3,6 +3,14 @@ package com.test.sort;
 public class QuickSort {
 	
 	// preusporada pole na prvky mensi nez pivot, pivot a prvky vetsi nez pivot
+	/**
+	 * 
+	 * @param list
+	 * @param left
+	 * @param right
+	 * @param pivot
+	 * @return
+	 */
 	public static int divide(int[] list, int left, int right, int pivot) {
 	  int temp = list[pivot]; // prohozeni pivotu s poslednim prvkem
 	  list[pivot] = list[right];
@@ -23,6 +31,12 @@ public class QuickSort {
 	  return iter; // vrati novy index pivotu
 	}
 
+	/**
+	 * 
+	 * @param list
+	 * @param left
+	 * @param right
+	 */
 	public static void limited_quicksort(int[] list, int left, int right) {
 	  if (right >= left) { // podminka rekurze
 	    int pivot = left; // vyber pivotu
@@ -33,11 +47,18 @@ public class QuickSort {
 	  }
 	}
 
-	// zavola omezeny quicksort na cele pole
+/**
+ * 
+ * @param list
+ */
 	public static void quicksort(int[] list) {
 	  limited_quicksort(list, 0, list.length - 1);
 	}
 	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args){
 	  int[] sortMe = {8,5,7,2,0,3,4,5,8,7,0,2,9,4,2};
 	  System.out.println(sortMe);
